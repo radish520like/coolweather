7732cde7312d15f2dd7c8b92e96c9ea3eba698a8
radish520like <radish520like@163.com>
2018-04-26 18:15:23 +0800

    增加后台自动更新天气的功能

--- app/src/main/java/com/coolweather/android/coolweather/WeatherActivity.java
+++ app/src/main/java/com/coolweather/android/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.coolweather.android.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -22,6 +23,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.coolweather.android.coolweather.gson.Forecast;
 import com.coolweather.android.coolweather.gson.Weather;
+import com.coolweather.android.coolweather.service.AutoUpdateService;
 import com.coolweather.android.coolweather.util.HttpUtil;
 import com.coolweather.android.coolweather.util.Utility;
 
@@ -59,7 +61,7 @@ public class WeatherActivity extends AppCompatActivity {
             来改变系统 UI 的显示
 
          */
-        if(Build.VERSION.SDK_INT >= 21){
+        if (Build.VERSION.SDK_INT >= 21) {
             View decorView = getWindow().getDecorView();
             //这里设置的参数的意思是表示活动的布局会显示在状态栏上面
             decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
@@ -73,7 +75,7 @@ public class WeatherActivity extends AppCompatActivity {
     /**
      * 初始化控件
      */
-    private void initView(){
+    private void initView() {
         scrollWeather = (ScrollView) findViewById(R.id.scrollview_weather);
         tvTitleCity = (TextView) findViewById(R.id.tv_title_city);
         tvUpdateTime = (TextView) findViewById(R.id.tv_update_time);
@@ -96,14 +98,14 @@ public class WeatherActivity extends AppCompatActivity {
         btnNav.setOnClickListener(v -> drawerLayout.openDrawer(GravityCompat.START));
 
         prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        String weatherString = prefs.getString("weather",null);
+        String weatherString = prefs.getString("weather", null);
         String weatherId;
         //有缓存，直接解析天气数据
-        if(!TextUtils.isEmpty(weatherString)){
+        if (!TextUtils.isEmpty(weatherString)) {
             Weather weather = Utility.handleWeatherResponse(weatherString);
             weatherId = weather.basic.weatherId;
             showWeatherInfo(weather);
-        }else{
+        } else {
             //无缓存从网络上获取
             weatherId = getIntent().getStringExtra("weather_id");
             //注意，请求数据的时候先将 ScrollView 隐藏掉，否则空数据的界面看上去很奇怪
@@ -111,11 +113,11 @@ public class WeatherActivity extends AppCompatActivity {
             requestWeather(weatherId);
         }
 
-        String bingPic = prefs.getString("bing_pic",null);
+        String bingPic = prefs.getString("bing_pic", null);
         //从缓存中读取，如果没有，就访问服务器
-        if(!TextUtils.isEmpty(bingPic)){
+        if (!TextUtils.isEmpty(bingPic)) {
             Glide.with(this).load(bingPic).into(ivBingPic);
-        }else{
+        } else {
             loadBingPic();
         }
 
@@ -125,14 +127,14 @@ public class WeatherActivity extends AppCompatActivity {
     /**
      * 从服务端获取背景图片
      */
-    private void loadBingPic(){
+    private void loadBingPic() {
         String requestBingPic = "http://guolin.tech/api/bing_pic";
         HttpUtil.sendOkHttpRequest(requestBingPic, new Callback() {
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 String bingPic = response.body().string();
                 SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(WeatherActivity.this).edit();
-                editor.putString("bing_pic",bingPic).apply();
+                editor.putString("bing_pic", bingPic).apply();
                 runOnUiThread(() -> Glide.with(WeatherActivity.this).load(bingPic).into(ivBingPic));
             }
 
@@ -154,12 +156,12 @@ public class WeatherActivity extends AppCompatActivity {
                 String responseText = response.body().string();
                 Weather weather = Utility.handleWeatherResponse(responseText);
                 runOnUiThread(() -> {
-                    if(weather != null && "ok".equals(weather.getStatus())){
+                    if (weather != null && "ok".equals(weather.getStatus())) {
                         SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(WeatherActivity.this).edit();
-                        editor.putString("weather",responseText);
+                        editor.putString("weather", responseText);
                         editor.apply();
                         showWeatherInfo(weather);
-                    }else{
+                    } else {
                         Toast.makeText(WeatherActivity.this, "获取天气信息失败", Toast.LENGTH_SHORT).show();
                     }
                     swipeRefresh.setRefreshing(false);
@@ -185,42 +187,50 @@ public class WeatherActivity extends AppCompatActivity {
      * 展示天气
      */
     private void showWeatherInfo(Weather weather) {
-        String cityName = weather.basic.cityName;
-        String updateTime = weather.basic.update.updateTime.split(" ")[1];
-        String degree = weather.now.temperature + "°C";
-        String weatherInfo = weather.now.more.info;
-        tvTitleCity.setText(cityName);
-        tvUpdateTime.setText(updateTime);
-        tvDegree.setText(degree);
-        tvWeatherInfo.setText(weatherInfo);
-        llForecast.removeAllViews();
-        /*
+        if (weather != null && "ok".equals(weather.status)) {
+            String cityName = weather.basic.cityName;
+            String updateTime = weather.basic.update.updateTime.split(" ")[1];
+            String degree = weather.now.temperature + "°C";
+            String weatherInfo = weather.now.more.info;
+            tvTitleCity.setText(cityName);
+            tvUpdateTime.setText(updateTime);
+            tvDegree.setText(degree);
+            tvWeatherInfo.setText(weatherInfo);
+            llForecast.removeAllViews();
+
+            /*
             这里处理每天的天气信息，在循环中动态加载 forecast_item.xml 布局并设置相应的数据，然后添加到
             父布局当中，设置完成后要把 ScrollView 变得可见
          */
-        for(Forecast forecast : weather.forecastList){
-            View view = LayoutInflater.from(this).inflate(R.layout.forecast_item, llForecast, false);
-            TextView tvDate = view.findViewById(R.id.tv_date);
-            TextView tvInfo = view.findViewById(R.id.tv_info);
-            TextView tvMax = view.findViewById(R.id.tv_max);
-            TextView tvMin = view.findViewById(R.id.tv_min);
-            tvDate.setText(forecast.date);
-            tvInfo.setText(forecast.more.info);
-            tvMax.setText(forecast.temperature.max);
-            tvMin.setText(forecast.temperature.min);
-            llForecast.addView(view);
-        }
-        if(weather.aqi != null){
-            tvAqi.setText(weather.aqi.city.aqi);
-            tvPm25.setText(weather.aqi.city.pm25);
-        }
+            for (Forecast forecast : weather.forecastList) {
+                View view = LayoutInflater.from(this).inflate(R.layout.forecast_item, llForecast, false);
+                TextView tvDate = view.findViewById(R.id.tv_date);
+                TextView tvInfo = view.findViewById(R.id.tv_info);
+                TextView tvMax = view.findViewById(R.id.tv_max);
+                TextView tvMin = view.findViewById(R.id.tv_min);
+                tvDate.setText(forecast.date);
+                tvInfo.setText(forecast.more.info);
+                tvMax.setText(forecast.temperature.max);
+                tvMin.setText(forecast.temperature.min);
+                llForecast.addView(view);
+            }
+            if (weather.aqi != null) {
+                tvAqi.setText(weather.aqi.city.aqi);
+                tvPm25.setText(weather.aqi.city.pm25);
+            }
 
-        String comfort = "舒适度："+weather.suggestion.comfort.info;
-        String carWash = "洗车指数："+weather.suggestion.carWash.info;
-        String sport = "运动建议："+weather.suggestion.sport.info;
-        tvComfort.setText(comfort);
-        tvCarWash.setText(carWash);
-        tvSport.setText(sport);
-        scrollWeather.setVisibility(View.VISIBLE);
+            String comfort = "舒适度：" + weather.suggestion.comfort.info;
+            String carWash = "洗车指数：" + weather.suggestion.carWash.info;
+            String sport = "运动建议：" + weather.suggestion.sport.info;
+            tvComfort.setText(comfort);
+            tvCarWash.setText(carWash);
+            tvSport.setText(sport);
+            scrollWeather.setVisibility(View.VISIBLE);
+
+            Intent intent = new Intent(this, AutoUpdateService.class);
+            startService(intent);
+        } else {
+            Toast.makeText(this, "获取天气信息失败", Toast.LENGTH_SHORT).show();
+        }
     }
 }
